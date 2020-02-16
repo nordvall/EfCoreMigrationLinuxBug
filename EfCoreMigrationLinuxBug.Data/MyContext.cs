f5be579691ed7ed9d342f768a4b2dc6aa4b5f6e3
nordvall <mattias@mnord.net>
2020-02-16 21:23:31 +0100

    Repro projects

---
+++ EfCoreMigrationLinuxBug.Data/MyContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfCoreMigrationLinuxBug.Data
+{
+    public class MyContext : DbContext
+    {
+        public MyContext()
+        { }
+
+        public MyContext(DbContextOptions<MyContext> options)
+            : base(options)
+        { }
+
+        public DbSet<MyEntity> MyEntities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            optionsBuilder.UseSqlite(connection);
+        }
+    }
+}
---
+++ EfCoreMigrationLinuxBug/Program.cs
@@ -0,0 +1,25 @@
+﻿using EfCoreMigrationLinuxBug.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EfCoreMigrationLinuxBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<MyContext>()
+                    .UseSqlite(connection)
+                    .Options;
+
+            using (var db = new MyContext(options))
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
